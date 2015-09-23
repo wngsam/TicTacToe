197d97ecd6bbd69d8236dfaebabd9693e870a7f8
Sam W. <wngsam@gmail.com>
2015-09-22 21:00:27 -0400

    Hard AI can block or seek victory.

--- src/tictactoe/AI.java
+++ src/tictactoe/AI.java
@@ -15,6 +15,7 @@
  */
 package tictactoe;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -34,8 +35,7 @@ public class AI {
         if(!challenge){
             return easyMove(gameState, moves);
         }else{
-            return easyMove(gameState, moves);
-            
+            return hardMove(gameState, prevMove, moves);
         }
     }
     
@@ -54,15 +54,121 @@ public class AI {
                 }
             }
         }
-        
         return -1;
     }
     
-    public int stub(Boolean[] gameState, int prevMove){
+    public int hardMove(Boolean[] gameState, int prevMove, int moves){
         
-        return -1;
+        ArrayList<Integer> available = new ArrayList();
+        ArrayList<Integer> myMoves = new ArrayList();
+        ArrayList<Integer> enemyMoves = new ArrayList();
+        
+        for(int i=0 ;i<gameState.length;i++){
+            if(gameState[i]==null){
+                available.add(i);
+            }else if(gameState[i]==false){
+                myMoves.add(i);
+            }else if(gameState[i]==true){
+                enemyMoves.add(i);
+            }
+        }
+        
+        for (Integer num : available) {
+            if (canWinOrBlock(num, myMoves)) {
+                return num;
+            }
+        }
+        
+        for (Integer num : available) {
+            if (canWinOrBlock(num, enemyMoves)) {
+                return num;
+            }
+        }
+        
+        return easyMove(gameState, moves);
     }
 
+    public boolean canWinOrBlock(int i, ArrayList<Integer> moves){
+        switch(i){
+            case 0: 
+                    if(moves.contains(1) && moves.contains(2)){
+                        return true;
+                    }else if(moves.contains(3) && moves.contains(6)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(8)){
+                        return true;
+                    }
+                    break;
+            case 1: 
+                    if(moves.contains(0) && moves.contains(2)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(7)){
+                        return true;
+                    }
+                    break;
+            case 2: 
+                    if(moves.contains(1) && moves.contains(0)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(6)){
+                        return true;
+                    }else if(moves.contains(5) && moves.contains(8)){
+                        return true;
+                    }
+                    break;
+            case 3: 
+                    if(moves.contains(0) && moves.contains(6)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(5)){
+                        return true;
+                    }
+                    break;
+            case 4: 
+                    if(moves.contains(0) && moves.contains(8)){
+                        return true;
+                    }else if(moves.contains(2) && moves.contains(6)){
+                        return true;
+                    }else if(moves.contains(3) && moves.contains(5)){
+                        return true;
+                    }else if(moves.contains(1) && moves.contains(7)){
+                        return true;
+                    }
+                    break;
+            case 5: 
+                    if(moves.contains(8) && moves.contains(2)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(3)){
+                        return true;
+                    }
+                    break;
+            case 6: 
+                    if(moves.contains(4) && moves.contains(2)){
+                        return true;
+                    }else if(moves.contains(3) && moves.contains(0)){
+                        return true;
+                    }else if(moves.contains(7) && moves.contains(8)){
+                        return true;
+                    }
+                    break;
+            case 7: 
+                    if(moves.contains(6) && moves.contains(8)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(1)){
+                        return true;
+                    }
+                    break;
+            case 8: 
+                    if(moves.contains(5) && moves.contains(2)){
+                        return true;
+                    }else if(moves.contains(7) && moves.contains(6)){
+                        return true;
+                    }else if(moves.contains(4) && moves.contains(0)){
+                        return true;
+                    }
+                    break;
+        }
+        return false;
+    }
+    
     public boolean isChallenge() {
         return challenge;
     }
